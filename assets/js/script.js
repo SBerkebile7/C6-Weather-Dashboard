179993051c081056b68d4003a1e6cc2663250c12
Spenny Berks <sberkebile@live.com>
2021-06-14 19:54:51 -0400

    initial commit with basic outlining

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Array holding cities chosen by user
+var chosenCity = [];
+var id = "";
+
+// Initializes the stored cities from localStorage
+function init() {
+
+}
+
+// Stores city list in localStorage
+function storeCity() {
+
+};
+
+// Allows searching of cities and adding them into the array
+function cityList() {
+
+};
+
+// Gathers current weather for city selected from the stored cities
+function cityCurrentWeather(currentCity, id) {
+
+}
+
+// Gathers 5-day forecast for chosen city
+function cityForecast(currentCity, id) {
+
+}
+
+// Gathers UV Index for current city and changes color based on conditions
+function UVIndex() {
+
+}
+
+// Displays current selected city
+function displayCurrentCity() {
+
+}
